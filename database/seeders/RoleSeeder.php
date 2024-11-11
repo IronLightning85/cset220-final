0e60b493a79044ac44bb13e2730b2248e59d0a5f
Peyton Meyer <peytonmeyer23@gmail.com>
2024-11-11 10:27:53 -0500

    Created RoleSeeder for the roles table
    php artisan db:seed --class=RoleSeeder
    This is used to add it to the database

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert([
+            ['role_name' => 'Admin', 'level' => 1],
+            ['role_name' => 'Supervisor', 'level' => 2],
+            ['role_name' => 'Doctor', 'level' => 3],
+            ['role_name' => 'Caregiver', 'level' => 4],
+            ['role_name' => 'Family', 'level' => 5],
+            ['role_name' => 'Patient', 'level' => 6],
+        ]);
+    }
+}
