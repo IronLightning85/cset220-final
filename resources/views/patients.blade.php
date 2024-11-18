40fee55bdff78593cfdca090778b7ca9cf79549a
Brennan Gochenaur <gochenaurbrennan@gmail.com>
2024-11-18 10:59:05 -0500

    Added Search Function on Patients Page

--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -38,13 +38,14 @@
 
         <tr>
             <form method="POST" action="{{ route('patient') }}">
+                @csrf
                 <td><button type="submit">Search</button></td>
                 <td><input type="text" name="patient_id" id="patient_id" placeholder = "Enter Patient ID"></td>
                 <td><input type="text" name="name" id="name" placeholder = "Enter Patient Name"></td>
                 <td><input type="text" name="age" id="age" placeholder = "Enter Patient Age"></td>
                 <td><input type="text" name="emergency_contact" id="emergency_contact" placeholder = "Enter Emergency Contact"></td>
                 <td><input type="text" name="emergency_contact_relation" id="emergency_contact_relation" placeholder = "Enter Emergency Relation"></td>
-                <td><input type="text" name="admission_date" id="admission_date" placeholder = "Enter Admission Date"></td>
+                <td><input type="date" name="admission_date" id="admission_date" placeholder = "Enter Admission Date"></td>
 
             </form>
         </tr>
